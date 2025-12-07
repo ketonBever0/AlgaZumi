cde4c80d0e590e4292f722ce05883ff47a0a4409
Kurucz Laci <lacikurucz09@gmail.com>
2025-12-07 14:42:59 +0100

    HackerRank - Marc's Cakewalk

---
+++ hackerrank/marcs_cakewalk.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+
+# START
+def marcsCakewalk(calorie):
+    # legnagyobbal celszeru kezdeni
+    calorie.sort(reverse=True)
+
+    # igy eleinte keveset setal, hogy ne egessen el tul sokat
+    current = 0
+    for i in range(len(calorie)):
+        current += calorie[i] * (2 ** i)
+
+    return current
+# END
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
